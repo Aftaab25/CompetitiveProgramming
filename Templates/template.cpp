faf31b9ea5cdf0865afc03464c5e52e04d41a972
aftaab25 <aftaab2507@gmail.com>
2024-11-05 16:15:31 +0530

    Add CPP Template

---
+++ Templates/template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << "Value of n is: " << endl;
+}
+
+int main() {
+	#ifdef ONLINEJUDGE
+	freopen("input.txt","r",stdin); 
+  freopen("output.txt","w",stdout); 
+  #endif
+
+  solve();
+
+	return 0;
+}
